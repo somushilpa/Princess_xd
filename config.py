089670d4e008ff2105e67fd7256879f8c6b41a7a
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-09-05 07:22:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # Add Owner Username without @ 
-OWNER_USERNAME = getenv("OWNER_USERNAME","Satya_Rajput_99")
+OWNER_USERNAME = getenv("OWNER_USERNAME","Baby_itachii")
 # Get Your bot username
 BOT_USERNAME = getenv("BOT_USERNAME" , "purvi_music_bot")
 # Don't Add style font 
@@ -29,7 +29,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", -1002182008630))
 
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6105670951))
+OWNER_ID = int(getenv("OWNER_ID", 6958410296))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -46,8 +46,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+PLTbaq9LInMxZjhl")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+PLTbaq9LInMxZjhl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PURVI_SUPPORT")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/CRAZY_OP_01")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
